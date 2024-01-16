b7c65263fc38c75b1b9fd6b02f69519664add1ef
Elton Mehmeti <97539294+EltonMehmeti@users.noreply.github.com>
2024-01-16 20:08:00 +0100

    Version 1.2 - Case Qkuk Cashflows insertion success

--- cardo/api/views.py
+++ cardo/api/views.py
@@ -6,7 +6,7 @@ import datetime as dt
 from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 from django.shortcuts import render
-from cardo.models import Cash_flows, Trade
+from cardo.models import Cash_flows, Trade,Operators
 from decimal import Decimal
 from cardo.api.serializers import TradeSerializer, CashFlowSerializer
 from ..forms import MappingForm
@@ -69,18 +69,98 @@ class MappingView(APIView):
             return Response("Please upload the trades file", status=400)
 
 
-
-class GetHeadExcelView(APIView):
+class CashflowView(APIView):
     parser_classes = (MultiPartParser,)
 
     def post(self, request, format=None):
         cashflow_file = request.FILES.get('cash_flows')
-        trade_file = request.FILES.get('trades')
 
         if cashflow_file:
-            df_trades1 = pd.read_excel(cashflow_file)
+            df_cashflows = pd.read_excel(cashflow_file)
+            df_cashflows['cashflow_date'] = pd.to_datetime(df_cashflows['cashflow_date'],
+                                                           format='%d/%m/%Y').dt.strftime('%Y-%m-%d')
+
+            cashflow_mapping = json.loads(request.data.get('cashflow_mapping', '{}'))
+
+            for index, row in df_cashflows.iterrows():
+                try:
+                    # Convert 'loan_identifier' to int
+                    row['loan_identifier'] = int(row['loan_identifier'])
+
+                    # Assuming the user provides a mapping for the loan identifier field
+                    loan_identifier_column = cashflow_mapping.get('loan_identifier', 'loan_identifier')
+                    operation_identifier_column = cashflow_mapping.get('cashflow_type', 'cashflow_type')
+
+                    # Fetch the Trade object using the loan identifier
+                    trade = Trade.objects.get(identifier=row[loan_identifier_column])
+
+                    # Determine the transaction type based on 'cashflow_type' and the sign of 'amount'
+                    cashflow_type = row[operation_identifier_column]
+                    amount = row['amount']
+
+                    if cashflow_type == 'cash_order':
+                        # Check the sign of 'amount' and set the transaction type accordingly
+                        if amount < 0:
+                            transaction_type = 'withdrawal'
+                        else:
+                            transaction_type = 'deposit'
+                    else:
+                        # For other 'cashflow_type', directly use 'cashflow_type' as transaction type
+                        transaction_type = cashflow_type
+
+                    # Query the Operators model based on the determined transaction type
+                    operation = Operators.objects.get(transaction_type=transaction_type)
+
+                    # Remove loan_identifier_column from the mapping since it's already used to get the Trade
+                    cashflow_mapping.pop('loan_identifier', None)
+
+                    # Remove trade_identifier and cashflow_type from the mapping if they exist
+                    cashflow_mapping.pop('trade_identifier', None)
+                    cashflow_mapping.pop('cashflow_type', None)
+
+                    try:
+                        # Continue with the original code
+                        cashflow_data = {model_field: row[excel_column] for model_field, excel_column in
+                                         cashflow_mapping.items()}
+                        cashflow_data['trade'] = trade
+                        cashflow_data['operation'] = operation
+                        cashflow = Cash_flows(**cashflow_data)
+                        cashflow.save()
+
+                    except Exception as e:
+                        print(f"An error occurred: {e}")
+
+                except Exception as e:
+                    print(e)
+
+            return Response("Cash flows uploaded successfully", status=200)
+
+        else:
+            return Response("Please upload the cashflows file", status=400)
+
+class GetTradeColumns(APIView):
+    parser_classes = (MultiPartParser,)
+
+    def post(self, request, format=None):
+        trade_file = request.FILES.get('trades')
+
+        if trade_file:
+            df_trades1 = pd.read_excel(trade_file)
 
             excel_file_columns = df_trades1.columns
+            print(excel_file_columns)
+
+            return Response(excel_file_columns, status=status.HTTP_200_OK)
+class GetCashflowColumns(APIView):
+    parser_classes = (MultiPartParser,)
+
+    def post(self, request, format=None):
+        cashflow_file = request.FILES.get('cashflows')
+
+        if cashflow_file:
+            df_cashflow = pd.read_excel(cashflow_file)
+
+            excel_file_columns = df_cashflow.columns
             print(excel_file_columns)
 
             return Response(excel_file_columns, status=status.HTTP_200_OK)
